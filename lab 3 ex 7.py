cd1c3d4ef936ecb6fe0ac4bc3df58dd66af7fd50
Guardain222 <84507580+Guardain222@users.noreply.github.com>
2022-10-13 16:10:42 +0100

    Add files via upload

--- lab 3 ex 7.py
+++ lab 3 ex 7.py
@@ -1,26 +1,20 @@
+
 import random
-player = {"Health is": 100 ,
- "Money is" : 200,
- "exp is": 100,
- "Player Alive" : True
-   }
-
-
-damage = int(input("Enter a number: "))
-
-def compute_experience(damage):
+player1 = {"Health" : 100 , "EXP" : 60 , "Money": 200, 1 : "One"}
+def damage_taken():
+    damage = int(input("Enter a number: "))
     b = damage * 2
     a = random.randint(0,b)
-    print("The player has taken", a, "ammount of damage")
+    c = 100 - a
+    d = 60 + a 
+    print ("The player has taken", a, "ammount of health")
+    damage_taken = a - 100
+    health_update = {"Health": c}
+    exp_update = {"EXP": d}
+    player1.update(health_update)
+    player1.update(exp_update)
+    print (player1)
 
-#compute_experience(damage)
-
-def take_damage(player, damage):
-    a = compute_experience(damage)
-    b = 
-    player ["Health"] = 100 - a
-    player
-    
-take_damage(player , damage )
+damage_taken()
 
 
